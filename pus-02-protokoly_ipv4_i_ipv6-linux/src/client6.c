ff21c93bd317f0b5d3b8b99cf85a74cfa7ce19b5
Michał Rotko <michalr835@gmail.com>
2018-04-02 18:40:36 +0200

    pus-02 zad04
    * implementacja serwera
    * implementacja klienta

--- pus-02-protokoly_ipv4_i_ipv6-linux/src/client6.c
+++ pus-02-protokoly_ipv4_i_ipv6-linux/src/client6.c
@@ -1,3 +1,86 @@
-int main() {
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <zconf.h>
 
+#define PORT 5432
+
+#define true 1
+#define false 0
+
+typedef int boolean;
+
+void validate(int val, const char *errmessage, boolean kill) {
+    if (val == -1) {
+        fprintf(stderr, "%s\n", errmessage);
+        if (kill == true) {
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+int getSocketFd() {
+    int sfd = socket(PF_INET6, SOCK_STREAM, IPPROTO_TCP);
+    validate(sfd, "socket()", true);
+    return sfd;
+}
+
+
+int main(int argc, char *argv[]) {
+    char                ipv6Addr[INET6_ADDRSTRLEN];
+    in_port_t           port;
+    unsigned int        if_index;
+    char                if_name[64];
+    int                 client_fd;
+    struct sockaddr_in6 srvr_addr;
+    socklen_t           srvr_addr_len;
+    int                 rval;
+    char                message[256];
+
+
+    if (argc != 4) {
+        fprintf(stderr, "<program> <server address> <port> <interface>\n");
+        exit(1);
+    }
+
+    strcpy(ipv6Addr, argv[1]);
+    port = (in_port_t) strtol(argv[2], 0, 10);
+    strcpy(if_name, argv[3]);
+
+
+    if_index = if_nametoindex(if_name);
+    if (if_index == 0) {
+        perror("if_nametoindex()\n");
+        exit(1);
+    }
+
+    client_fd = getSocketFd();
+
+
+    memset(&srvr_addr, 0, sizeof(srvr_addr));
+    inet_pton(AF_INET6, ipv6Addr, &(srvr_addr.sin6_addr));
+    srvr_addr.sin6_family   = AF_INET6;
+    srvr_addr.sin6_port     = htons(port);
+    srvr_addr.sin6_scope_id = if_index;
+    srvr_addr_len = sizeof(srvr_addr);
+
+    fprintf(stderr, "Client data: \n");
+    fprintf(stderr, "Address:   %s\n", ipv6Addr);
+    fprintf(stderr, "Port:      %d\n", port);
+    fprintf(stderr, "Interface: %d\n", if_index);
+    fprintf(stderr, "************************************\n");
+
+    rval = connect(client_fd, (const struct sockaddr *) &srvr_addr, srvr_addr_len);
+    validate(rval, "connect()", true);
+
+    memset(message, 0, sizeof(message));
+    recv(client_fd, message, 255, 0);
+    message[255] = '\0';
+
+    fprintf(stderr, "Nawiazano polaczenie. Oto wiadomosc serwera: %s\n", message);
+
+    close(client_fd);
 }
--- pus-02-protokoly_ipv4_i_ipv6-linux/src/server4.c
+++ pus-02-protokoly_ipv4_i_ipv6-linux/src/server4.c
@@ -1,3 +1,128 @@
-int main() {
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 
+#define PORT 5432
+
+#define true 1
+#define false 0
+
+typedef int boolean;
+
+void validate(int val, const char *errmessage, boolean kill) {
+    if (val == -1) {
+        fprintf(stderr, "%s\n", errmessage);
+        if (kill == true) {
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+int getSocketFd() {
+    int sfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    validate(sfd, "socket()", true);
+    return sfd;
+}
+
+int getBindedFd() {
+    int                sfd, rval;
+    struct sockaddr_in srvr_addr;
+    socklen_t          srvr_addr_len;
+
+    sfd = getSocketFd();
+    memset(&srvr_addr, 0, sizeof(struct sockaddr));
+    srvr_addr.sin_family      = AF_INET;
+    srvr_addr.sin_port        = htons(PORT);
+    srvr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    srvr_addr_len = sizeof(srvr_addr);
+
+    rval = bind(sfd, (const struct sockaddr *) &srvr_addr, srvr_addr_len);
+    validate(rval, "bind()", true);
+
+    return sfd;
+}
+
+char *getIpv4PAddr(int sfd, char *buff, int *port) {
+    struct sockaddr_in addripv4;
+    socklen_t          addripv4_len = sizeof(addripv4);
+    int                rval;
+
+    memset(&addripv4, 0, addripv4_len);
+    rval = getsockname(sfd, (struct sockaddr *) &addripv4, &addripv4_len);
+    validate(rval, "error ipv4 getsockname()", false);
+
+    inet_ntop(AF_INET, &(addripv4.sin_addr), buff, INET_ADDRSTRLEN);
+    *(port) = ntohs(addripv4.sin_port);
+
+    return buff;
+}
+
+void printServerData(int sfd) {
+    char paddripv4[INET_ADDRSTRLEN];
+    int  port;
+
+    getIpv4PAddr(sfd, paddripv4, &port);
+
+    fprintf(stderr, "Server connection data: \n");
+    fprintf(stderr, "Ipv4 addr: %s\n", paddripv4);
+    fprintf(stderr, "Port:      %d\n", port);
+    fprintf(stderr, "***************************************\n");
+}
+
+void printClientData(struct sockaddr_in *sockaddripv4) {
+    char paddripv4[INET_ADDRSTRLEN];
+    int  port;
+
+    inet_ntop(AF_INET, &(sockaddripv4->sin_addr), paddripv4, INET_ADDRSTRLEN);
+    port = ntohs(sockaddripv4->sin_port);
+
+    fprintf(stderr, "Polaczenie z nowym klientem\n");
+    fprintf(stderr, "Adres ipv4: %s\n", paddripv4);
+    fprintf(stderr, "Port:       %d\n", port);
+}
+
+int getListenFd() {
+    int sfd, rval;
+
+    sfd  = getBindedFd();
+    rval = listen(sfd, 1);
+    validate(rval, "listen()", true);
+
+    printServerData(sfd);
+
+    return sfd;
+}
+
+void sendReturnMessage(int connfd) {
+    char    message[] = "Laboratorium PUS";
+    ssize_t rval;
+
+    rval = send(connfd, message, sizeof(message), 0);
+    validate((int) rval, "send()", true);
+
+    fprintf(stderr, "Wyslano wiadomosc: %s\n", message);
+}
+
+int main() {
+    int listen_fd;
+
+    struct sockaddr_in client_addr;
+    socklen_t          client_addr_len;
+
+    listen_fd = getListenFd();
+
+    while (true) {
+        int connfd;
+        memset(&client_addr, 0, sizeof(struct sockaddr_in));
+        client_addr_len = sizeof(struct sockaddr_in);
+
+        connfd = accept(listen_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+        validate(connfd, "accept()", true);
+
+        printClientData(&client_addr);
+        sendReturnMessage(connfd);
+        fprintf(stderr, "***************************************\n");
+    }
 }
