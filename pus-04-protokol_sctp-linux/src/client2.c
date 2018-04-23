7f98be8dc8065ffaa1604bb88d9408adb7799101
Michał Rotko <michalr835@gmail.com>
2018-04-24 01:52:11 +0200

    pus-04 - zad 02
    * serwer sctp wysyła datę i godzinę w dwóch wiadomościach, a następnie zamyka połączenie
    * klient sctp nawiązuje połączenie i czeka na wiadomości, które serwer wysyła zaraz po połączeniu, odbiera wiadomości dopóki serwer nie zakończy połączenia

--- pus-04-protokol_sctp-linux/src/client2.c
+++ pus-04-protokol_sctp-linux/src/client2.c
@@ -1,3 +1,124 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#include <zconf.h>
+#include <time.h>
+#include <memory.h>
+#include <arpa/inet.h>
 
+#define TIME 0
+#define DATE 1
+
+#define true 1
+#define false 0
+
+#define MESSAGE_SIZE 100
+
+
+typedef unsigned short boolean;
+
+typedef struct {
+    unsigned short type;
+    char           message[MESSAGE_SIZE];
+}                      message_t;
+
+void test(int rval, const char *message, boolean kill);
+
+void socket_w(int *fd);
+
+void connect_w(const int *fd, struct sockaddr_in *server_addr);
+
+void print_conn_info(const int *fd);
+
+void handle_connection(const int *fd, struct sockaddr_in *server_addr);
+
+int main(int argc, char *argv[]) {
+    int                port;
+    int                buff[100];
+    char               *server_ip;
+    int                fd;
+    int                retval;
+    struct sockaddr_in server_addr;
+    if (argc != 3) {
+        fprintf(stderr, "<program> <ip> <port>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    server_ip = argv[1];
+    port      = (int) strtol(argv[2], 0, 10);
+
+    socket_w(&fd);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port   = htons((uint16_t) port);
+    retval = inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
+    test(retval, "inet_pton()", true);
+
+    connect_w(&fd, &server_addr);
+    printf("Nawiązano połączenie z serwerem. Oczekiwanie na wiadomość\n");
+    print_conn_info(&fd);
+
+    handle_connection(&fd, &server_addr);
+}
+
+void handle_connection(const int *fd, struct sockaddr_in *server_addr) {
+    message_t rcv_msg;
+    socklen_t server_addr_len = sizeof(struct sockaddr_in);
+    int       retval;
+    while (1) {
+        memset(&rcv_msg, 0, sizeof(message_t));
+        retval = sctp_recvmsg(*fd, &rcv_msg, sizeof(message_t), (struct sockaddr *) server_addr, &server_addr_len, 0,
+                              0);
+        test(retval, "sctp_recvmsg()", true);
+        if (retval == 0) {
+            printf("Serwer zakończył połączenie\n");
+            break;
+        }
+
+        if (rcv_msg.type == DATE) {
+            printf("Serwer wysłał datę:    %s\n", rcv_msg.message);
+        } else if (rcv_msg.type == TIME) {
+            printf("Serwer wysłał godzinę: %s\n", rcv_msg.message);
+        } else {
+            printf("Nieznany typ wiadomości\n");
+        }
+    }
+}
+
+void print_conn_info(const int *fd) {
+    struct sctp_status status;
+    socklen_t          status_len = sizeof(struct sctp_status);
+    int                retval;
+
+    retval = getsockopt(*fd, IPPROTO_SCTP, SCTP_STATUS, &status, &status_len);
+    test(retval, "getsockopt()", true);
+
+    printf("*****  Dane połączenia   *****\n");
+    printf("Id:                        %d\n", status.sstat_assoc_id);
+    printf("Stan:                      %d\n", status.sstat_state);
+    printf("Strumienie wychodzące:     %d\n", status.sstat_outstrms);
+    printf("Strumienie przychodzące:   %d\n", status.sstat_instrms);
+    printf("*************************************\n");
+}
+
+void connect_w(const int *fd, struct sockaddr_in *server_addr) {
+    int retval = connect(*fd, (const struct sockaddr *) server_addr, sizeof(*server_addr));
+    test(retval, "connect()", true);
+}
+
+void socket_w(int *fd) {
+    *fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    test(*fd, "socket()", true);
+}
+
+void test(int rval, const char *message, boolean kill) {
+    if (rval == -1) {
+        perror(message);
+
+        if (kill == true) {
+            exit(EXIT_FAILURE);
+        }
+    }
 }
--- pus-04-protokol_sctp-linux/src/server2.c
+++ pus-04-protokol_sctp-linux/src/server2.c
@@ -1,3 +1,157 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#include <zconf.h>
+#include <time.h>
+#include <memory.h>
 
+#define TIME 0
+#define DATE 1
+
+#define true 1
+#define false 0
+
+#define MESSAGE_SIZE 100
+
+
+typedef unsigned short boolean;
+
+typedef struct {
+    unsigned short type;
+    char           message[MESSAGE_SIZE];
+}                      message_t;
+
+void test(int rval, const char *message, boolean kill);
+
+void socket_w(int *fd);
+
+void bind_w(const int *fd, int port);
+
+void listen_w(const int *fd);
+
+int accept_w(const int *fd, struct sockaddr_in *client_addr);
+
+void sockopt_config(const int *fd);
+
+void send_messages(const int *fd, struct sockaddr_in *client_addr);
+
+int main(int argc, char *argv[]) {
+    int port;
+    int fd;
+    if (argc != 2) {
+        fprintf(stderr, "<program> <port>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    port = (int) strtol(argv[1], 0, 10);
+
+    socket_w(&fd);
+    bind_w(&fd, port);
+    listen_w(&fd);
+    sockopt_config(&fd);
+
+    printf("Gniazdo skonfigurowane\n");
+    while (1) {
+        struct sockaddr_in client_addr;
+        int                conn_fd = accept_w(&fd, &client_addr);
+        send_messages(&conn_fd, &client_addr);
+        close(conn_fd);
+    }
+}
+
+void send_messages(const int *fd, struct sockaddr_in *client_addr) {
+    time_t current_time;
+    struct tm *current_time_tm;
+    char      time_buff[MESSAGE_SIZE];
+    char      date_buff[MESSAGE_SIZE];
+    int       retval;
+    message_t message_date, message_time;
+
+    memset(time_buff, 0, MESSAGE_SIZE);
+    memset(date_buff, 0, MESSAGE_SIZE);
+
+    time(&current_time);
+    current_time_tm = localtime(&current_time);
+
+    strftime(time_buff, MESSAGE_SIZE, "%R", current_time_tm);
+    strftime(date_buff, MESSAGE_SIZE, "%F", current_time_tm);
+
+    printf("Aktualna data:    %s\n", date_buff);
+    printf("Aktualna godzina: %s\n", time_buff);
+
+    message_time.type = TIME;
+    memset(message_date.message, 0, MESSAGE_SIZE);
+    strcpy(message_time.message, time_buff);
+
+
+    message_date.type = DATE;
+    memset(message_date.message, 0, MESSAGE_SIZE);
+    strcpy(message_date.message, date_buff);
+
+    retval = sctp_sendmsg(*fd, &message_time, sizeof(message_t), (struct sockaddr *) client_addr, sizeof(*client_addr),
+                          (uint32_t) getpid(), 0, 0, 0, 0);
+    test(retval, "send time error", false);
+
+
+    retval = sctp_sendmsg(*fd, &message_date, sizeof(message_t), (struct sockaddr *) client_addr, sizeof(*client_addr),
+                          (uint32_t) getpid(), 0, 0, 0, 0);
+    test(retval, "send date error", false);
+}
+
+
+int accept_w(const int *fd, struct sockaddr_in *client_addr) {
+    int conn_fd;
+    printf("Oczekiwnaie na klienta\n");
+    socklen_t socklen = sizeof(struct sockaddr_in);
+    conn_fd = accept(*fd, (struct sockaddr *) client_addr, &socklen);
+    test(conn_fd, "accept()", true);
+    printf("Nazwiązano połączenie z nowym klientem.\nWysyłanie danych\n\n");
+    return conn_fd;
+}
+
+void sockopt_config(const int *fd) {
+    struct sctp_initmsg initmsg;
+
+    initmsg.sinit_num_ostreams  = 3;
+    initmsg.sinit_max_instreams = 4;
+    initmsg.sinit_max_attempts  = 5;
+
+    setsockopt(*fd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+}
+
+
+void listen_w(const int *fd) {
+    int rval;
+    rval = listen(*fd, 5);
+
+    test(rval, "listen()", true);
+}
+
+void bind_w(const int *fd, int port) {
+    struct sockaddr_in server_addr;
+    int                rval;
+
+    server_addr.sin_port        = htons((uint16_t) port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family      = AF_INET;
+    rval = bind(*fd, (const struct sockaddr *) &server_addr, sizeof(struct sockaddr_in));
+
+    test(rval, "bind()", true);
+}
+
+void socket_w(int *fd) {
+    *fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    test(*fd, "socket()", true);
+}
+
+void test(int rval, const char *message, boolean kill) {
+    if (rval == -1) {
+        perror(message);
+
+        if (kill == true) {
+            exit(EXIT_FAILURE);
+        }
+    }
 }
