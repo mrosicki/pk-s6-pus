cf1a9367362404cd15ffb1e49845443efd360610
Michal <michal.rosicki@gmail.com>
2018-05-04 16:15:31 +0200

    Zrobienie zadania 2 i 5, do sprawdzenia zerowanie uprawnien. zostaje 0000000400 a nie powinno byc 0000000000?

--- pus-08-uprawnienia-linux/src/capserver.c
+++ pus-08-uprawnienia-linux/src/capserver.c
@@ -1,3 +1,110 @@
-int main() {
+/*
+ * Data:                2009-04-25
+ * Autor:               Jakub Gasior <quebes@mars.iti.pk.edu.pl>
+ * Kompilacja:          $ gcc suidserver.c -o suidserver
+ * Uruchamianie:        $ ./suidserver <numer portu>
+ */
 
+#define _GNU_SOURCE     /* getresgid() */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> /* socket() */
+#include <netinet/in.h> /* struct sockaddr_in */
+#include <arpa/inet.h>  /* inet_ntop() */
+#include <unistd.h>     /* close() */
+#include <string.h>
+#include <errno.h>
+#include <sys/capability.h>
+
+int main(int argc, char** argv) {
+
+    /* Deskryptory dla gniazda nasluchujacego i polaczonego: */
+    int             listenfd, connfd,retval;
+
+    /* Gniazdowe struktury adresowe (dla klienta i serwera): */
+    struct          sockaddr_in client_addr, server_addr;
+
+    /* Rozmiar struktur w bajtach: */
+    socklen_t       client_addr_len, server_addr_len;
+
+    /* Bufor dla adresu IP klienta w postaci kropkowo-dziesietnej: */
+    char            addr_buff[256];
+
+    unsigned int    port_number;
+
+    cap_t           capabilities;
+
+    if (argc != 2) {
+        fprintf(stderr, "Invocation: %s <PORT>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    port_number = atoi(argv[1]);
+
+    /* Utworzenie gniazda dla protokolu TCP: */
+    listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (listenfd == -1) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Wyzerowanie struktury adresowej serwera: */
+    memset(&server_addr, 0, sizeof(server_addr));
+    /* Domena komunikacyjna (rodzina protokolow): */
+    server_addr.sin_family          =       AF_INET;
+    /* Adres nieokreslony (ang. wildcard address): */
+    server_addr.sin_addr.s_addr     =       htonl(INADDR_ANY);
+    /* Numer portu: */
+    server_addr.sin_port            =       htons(port_number);
+    /* Rozmiar struktury adresowej serwera w bajtach: */
+    server_addr_len                 =       sizeof(server_addr);
+
+    fprintf(stdout, "Binding to port %u...\n", port_number);
+    /* Powiazanie "nazwy" (adresu IP i numeru portu) z gniazdem: */
+    if (bind(listenfd, (struct sockaddr*) &server_addr, server_addr_len) == -1) {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Przeksztalcenie gniazda w gniazdo nasluchujace: */
+    if (listen(listenfd, 2) == -1) {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "Server is listening for incoming connection...\n");
+
+    /* Funkcja pobiera polaczenie z kolejki polaczen oczekujacych na zaakceptowanie
+     * i zwraca deskryptor dla gniazda polaczonego: */
+    client_addr_len = sizeof(client_addr);
+    connfd = accept(listenfd, (struct sockaddr*)&client_addr, &client_addr_len);
+    if (connfd == -1) {
+        perror("accept()");
+        exit(EXIT_FAILURE);
+    }
+    capabilities = cap_init();
+    cap_clear_flag(capabilities,CAP_EFFECTIVE);
+    cap_clear_flag(capabilities,CAP_INHERITABLE);
+    cap_clear_flag(capabilities,CAP_PERMITTED);
+    cap_clear(capabilities);
+    retval = cap_set_proc(capabilities);
+    cap_free(capabilities);
+    if (retval != 0) {
+        perror("cap_set_proc()");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(
+            stdout, "TCP connection accepted from %s:%d\n",
+            inet_ntop(AF_INET, &client_addr.sin_addr, addr_buff, sizeof(addr_buff)),
+            ntohs(client_addr.sin_port)
+    );
+
+    sleep(5);
+
+    fprintf(stdout, "Closing connected socket (sending FIN to client)...\n");
+    close(connfd);
+
+    close(listenfd);
+
+    exit(EXIT_SUCCESS);
 }
