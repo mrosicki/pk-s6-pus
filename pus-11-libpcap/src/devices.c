8f7053b6e0b624f01ec2b4747532084c24e94c0f
Michał Rotko <michalr835@gmail.com>
2018-05-23 10:32:52 +0200

    PUS 11: Program 1 #41
    * dodanie inet_ntop

--- pus-11-libpcap/src/devices.c
+++ pus-11-libpcap/src/devices.c
@@ -13,6 +13,8 @@
 #include <netinet/ether.h>      /* ether_ntoa() */
 #include <netpacket/packet.h>   /* struct sockaddr_ll */
 #include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
 
 
 int main(int argc, char **argv) {
@@ -79,14 +81,27 @@ int main(int argc, char **argv) {
                 err = getnameinfo(addr_t->addr, addr_len,
                                   buf, NI_MAXHOST, NULL, 0,
                                   NI_NUMERICHOST | NI_NUMERICSERV);
+
                 if (err != 0) {
                     fprintf(stderr, "getnameinfo(): %s\n",
                             gai_strerror(err));
 
                     exit(EXIT_FAILURE);
                 }
+                fprintf(stdout, "IP address getnameinfo: %s\n", buf);
 
-                fprintf(stdout, "IP address: %s\n", buf);
+
+                if(family == AF_INET) {
+                    memset(buf, 0, NI_MAXHOST);
+                    inet_ntop(AF_INET, &(((struct sockaddr_in *) (addr_t->addr))->sin_addr), buf, NI_MAXHOST);
+
+                } else {
+                    memset(buf, 0, NI_MAXHOST);
+                    inet_ntop(AF_INET6, &(((struct sockaddr_in6 *) (addr_t->addr))->sin6_addr), buf, NI_MAXHOST);
+                }
+
+
+                fprintf(stdout, "IP address ntop:        %s\n", buf);
 
             } else if (family == AF_PACKET) {
 
