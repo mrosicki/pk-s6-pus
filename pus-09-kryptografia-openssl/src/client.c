fece4cba143c014aa3ccb4ff5dad477bcd131098
Michał Rotko <michalr835@gmail.com>
2018-06-13 22:21:03 +0200

    #57 PUS 12: Program 6
    Szyfrowana komunikacja klient-serwer

--- pus-09-kryptografia-openssl/src/client.c
+++ pus-09-kryptografia-openssl/src/client.c
@@ -12,7 +12,97 @@
 #include <arpa/inet.h>  /* inet_pton() */
 #include <unistd.h>     /* close() */
 #include <string.h>
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
 #include <errno.h>
+#include <openssl/err.h>
+
+/* Klucz: */
+unsigned char key[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
+                       0x00,0x01,0x02,0x03,0x04,0x05
+};
+
+/* Wektor inicjalizacyjny: */
+unsigned char iv[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
+                      0x00,0x01,0x02,0x03,0x04,0x05
+};
+
+void print_hex(const char * message, int len) {
+    int i;
+    for (i= 0; i < len; i++) {
+        fprintf(stdout, "%02x", (unsigned char)message[i]);
+    }
+    fprintf(stdout, "\n");
+}
+
+void hmac(char * buff, char * key,  const unsigned char * message) {
+    HMAC_CTX *hmac_ctx;
+    hmac_ctx = (HMAC_CTX *) malloc(sizeof(HMAC_CTX));
+    const EVP_MD *md;
+    unsigned char hmac_mac[256];
+    memset(hmac_mac, 0, 256);
+    unsigned int hmac_mac_len;
+
+    ERR_load_crypto_strings();
+    OpenSSL_add_all_digests();
+
+    md = EVP_get_digestbyname("md5");
+
+    HMAC_CTX_init(hmac_ctx);
+    HMAC_Init(hmac_ctx, key, 16, md);
+
+    HMAC_Update(hmac_ctx, message, strlen(message));
+
+    HMAC_Final(hmac_ctx, hmac_mac, &hmac_mac_len);
+
+    fprintf(stdout, "HMAC_mac: ", hmac_mac);
+    print_hex(hmac_mac, hmac_mac_len);
+    fprintf(stdout, "HMAC_mac_len: %d\n", hmac_mac_len);
+
+    memcpy(buff, hmac_mac, 16);
+    memcpy(buff + 16, message, strlen(message));
+
+    HMAC_CTX_cleanup(hmac_ctx);
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
+void encrypt_cipher(char *buff, char *key, char *message) {
+    EVP_CIPHER_CTX *ctx;
+    const EVP_CIPHER* cipher;
+    ERR_load_crypto_strings();
+    ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));
+    EVP_CIPHER_CTX_init(ctx);
+    cipher = EVP_aes_128_ecb();
+    int retval = EVP_EncryptInit_ex(ctx, cipher, NULL, key, NULL);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    int buff_len = strlen(message) + 20;
+    EVP_CIPHER_CTX_set_padding(ctx, 1);
+    printf("Message to encrypt: %s\n", message);
+    retval = EVP_EncryptUpdate(ctx, buff, &buff_len, message, strlen(message));
+
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    int  tmp;
+    retval = EVP_EncryptFinal_ex(ctx, buff + buff_len, &tmp);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    buff[buff_len + tmp] = '\0';
+
+    EVP_CIPHER_CTX_cleanup(ctx);
+    ERR_free_strings();
+
+    printf("Cipher: ");
+    print_hex(buff, buff_len + tmp);
+}
+
 
 int main(int argc, char** argv) {
 
@@ -21,8 +111,11 @@ int main(int argc, char** argv) {
     struct          sockaddr_in remote_addr;/* Gniazdowa struktura adresowa. */
     socklen_t       addr_len;               /* Rozmiar struktury w bajtach. */
 
-    char            buff[256] = "Laboratorium PUS.";
-
+    char            message[256] = "Laboratorium PUS.";
+    char hmac_message[256];
+    memset(hmac_message, 0, 256);
+    char cipher_message[256];
+    memset(cipher_message, 0, 256);
 
     if (argc != 3) {
         fprintf(stderr, "Invocation: %s <IPv4 ADDRESS> <PORT>\n", argv[0]);
@@ -57,12 +150,18 @@ int main(int argc, char** argv) {
     fprintf(stdout, "Sending message to %s.\n", argv[1]);
 
     /* sendto() wysyla dane na adres okreslony przez strukture 'remote_addr': */
-    retval = sendto(
-                 sockfd,
-                 buff, strlen(buff),
-                 0,
-                 (struct sockaddr*)&remote_addr, addr_len
-             );
+
+    hmac(hmac_message, key, message);
+    encrypt_cipher(cipher_message, key, hmac_message);
+
+
+    retval = (int) sendto(
+                     sockfd,
+                     cipher_message, strlen(cipher_message),
+                     0,
+                     (struct sockaddr*)&remote_addr, addr_len
+                 );
+
 
     if (retval == -1) {
         perror("sendto()");
--- pus-09-kryptografia-openssl/src/server.c
+++ pus-09-kryptografia-openssl/src/server.c
@@ -13,6 +13,98 @@
 #include <unistd.h>     /* close() */
 #include <string.h>
 #include <errno.h>
+#include <openssl/hmac.h>
+#include <openssl/err.h>
+
+/* Klucz: */
+unsigned char key[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
+                       0x00,0x01,0x02,0x03,0x04,0x05
+};
+
+/* Wektor inicjalizacyjny: */
+unsigned char iv[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
+                      0x00,0x01,0x02,0x03,0x04,0x05
+};
+
+void print_hex(const char * message, int len) {
+    int i;
+    for (i= 0; i < len; i++) {
+        fprintf(stdout, "%02x", (unsigned char)message[i]);
+    }
+    fprintf(stdout, "\n");
+}
+
+void hmac(char * buff, char * key,  const unsigned char * message) {
+    HMAC_CTX *hmac_ctx;
+    hmac_ctx = (HMAC_CTX *) malloc(sizeof(HMAC_CTX));
+    const EVP_MD *md;
+    unsigned char hmac_mac[256];
+    unsigned int hmac_mac_len;
+
+    ERR_load_crypto_strings();
+    OpenSSL_add_all_digests();
+
+    md = EVP_get_digestbyname("md5");
+
+    HMAC_CTX_init(hmac_ctx);
+    HMAC_Init(hmac_ctx, key, 16, md);
+
+    HMAC_Update(hmac_ctx, message, strlen(message));
+
+    HMAC_Final(hmac_ctx, hmac_mac, &hmac_mac_len);
+
+    memcpy(buff, hmac_mac, 16);
+
+    HMAC_CTX_cleanup(hmac_ctx);
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
+void decrypt_cipher(char *buff, char *key, char *message) {
+    EVP_CIPHER_CTX *ctx;
+    const EVP_CIPHER* cipher;
+    ERR_load_crypto_strings();
+    ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));
+    EVP_CIPHER_CTX_init(ctx);
+    cipher = EVP_aes_128_ecb();
+    int retval = EVP_DecryptInit_ex(ctx, cipher, NULL, key, NULL);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    int buff_len = strlen(message);
+
+    EVP_CIPHER_CTX_set_padding(ctx, 1);
+    retval = EVP_DecryptUpdate(ctx, buff, &buff_len, message, strlen(message));
+
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    int tmp;
+    retval = EVP_DecryptFinal_ex(ctx, (unsigned char*)buff + buff_len, &tmp);
+    buff[buff_len + tmp] = '\0';
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_CIPHER_CTX_cleanup(ctx);
+    ERR_free_strings();
+}
+
+
+void get_hmac_from_message(char * buff, char *message) {
+    memset(buff, 0, 16);
+    strncpy(buff, message, 16);
+}
+
+void remove_hmac_from_message(char * buff, char *message) {
+    int message_len = strlen(message) - 16 + 1;
+    memset(buff, 0, message_len);
+    strncpy(buff, message + 16, message_len);
+}
 
 int main(int argc, char** argv) {
 
@@ -26,7 +118,8 @@ int main(int argc, char** argv) {
     socklen_t       client_addr_len, server_addr_len;
 
     /* Bufor wykorzystywany przez recvfrom(): */
-    char            buff[256];
+    char            cipher_buff[256];
+    memset(cipher_buff, 0, 256);
 
     /* Bufor dla adresu IP klienta w postaci kropkowo-dziesietnej: */
     char            addr_buff[256];
@@ -66,12 +159,13 @@ int main(int argc, char** argv) {
     client_addr_len = sizeof(client_addr);
 
     /* Oczekiwanie na dane od klienta: */
-    retval = recvfrom(
-                 sockfd,
-                 buff, sizeof(buff),
-                 0,
-                 (struct sockaddr*)&client_addr, &client_addr_len
-             );
+
+    retval = (int) recvfrom(
+                     sockfd,
+                     cipher_buff, sizeof(cipher_buff),
+                     0,
+                     (struct sockaddr*)&client_addr, &client_addr_len
+                 );
     if (retval == -1) {
         perror("recvfrom()");
         exit(EXIT_FAILURE);
@@ -82,8 +176,36 @@ int main(int argc, char** argv) {
             ntohs(client_addr.sin_port)
            );
 
-    fprintf(stdout, "Message: ");
-    fwrite(buff, sizeof(char), retval, stdout);
+    char plain_text_buff[256];
+    char hmac_buff[256];
+    char message_buff[256];
+    char hmac_calculated_buff[256];
+
+    memset(plain_text_buff, 0, 256);
+    memset(hmac_buff, 0, 256);
+    memset(message_buff, 0, 256);
+    memset(hmac_calculated_buff, 0, 256);
+
+    decrypt_cipher(plain_text_buff, key, cipher_buff);
+    get_hmac_from_message(hmac_buff, plain_text_buff);
+    remove_hmac_from_message(message_buff, plain_text_buff);
+    hmac(hmac_calculated_buff, key, message_buff);
+
+    fprintf(stdout, "Cipher: ");
+    print_hex(cipher_buff, strlen(cipher_buff));
+
+    fprintf(stdout, "Plain text: ");
+    fwrite(plain_text_buff, sizeof(char), retval, stdout);
+    fprintf(stdout, "\n");
+
+    fprintf(stdout, "Hmac received: ");
+    print_hex(hmac_buff, strlen(hmac_buff));
+
+    fprintf(stdout, "Hmac calculated: ");
+    print_hex(hmac_calculated_buff, strlen(hmac_calculated_buff));
+
+    fprintf(stdout, "Decrypted message: ");
+    fwrite(message_buff, sizeof(char), retval, stdout);
     fprintf(stdout, "\n");
 
     close(sockfd);
